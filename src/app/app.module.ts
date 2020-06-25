fc66479dcf5baf692fe8a4a33947b5c6e40ebc0a
kushaalrana <ranakushaal@gmail.com>
2020-06-25 22:34:17 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { MatSliderModule } from '@angular/material';
 import { ProcessHTTPMsgService} from './services/process-httpmsg.service';
 
 import {baseURL} from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import {baseURL} from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
